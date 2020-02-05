9f885c7f61adf995f4740dcc083221904a9e62bc
2chevskii <prsroman3@gmail.com>
2020-02-05 20:17:49 +0300

    RustInfoDump script testing

--- RustInfoDump.ps1
+++ RustInfoDump.ps1
@@ -14,7 +14,7 @@ if (!$out) {
     $out = $config.rustinfodump.'out-dir'
 }
 
-if (!(Test-Path "$server/RustDedicated.exe")) {
+if (!($IsWindows -and (Test-Path "$server/RustDedicated.exe")) -or !((Test-Path "$server/RustDedicated") -and $IsLinux)) {
     Write-Error 'Could not find RustDedicated executable! Verify configuration and try again'
     exit 1
 }
@@ -29,8 +29,35 @@ if (!(Test-Path -Path './RustDataDump.cs')) {
     exit 1
 }
 
+# Make sure out directory exists
 New-Item -Path $out -ItemType Directory -ErrorAction SilentlyContinue
 
+## Copy plugin into server dir
 Copy-Item -Path './RustDataDump.cs' -Destination "$server/oxide/plugins" -Recurse -Force
 
-## launch server and stuff (placeholder)
+if ($IsWindows) {
+    Start-Process -FilePath "$server/RustDedicated.exe" -Wait
+}
+else {
+    Start-Process -FilePath "$server/RustDedicated" -Wait
+}
+
+if (!(Test-Path -Path "$server/oxide/logs/RustDataDump") -or (Get-ChildItem "$server/oxide/logs/RustDataDump").Length -lt 1) {
+    Write-Error "Dump was not successfull, try again later!"
+    exit 1
+}
+
+Get-ChildItem "$server/oxide/logs/RustDataDump" | Sort-Object LastWriteTime -OutVariable lastdump
+
+$hashfunc = [System.Security.Cryptography.MD5]::Create()
+
+$dumpcontent = Get-Content $lastdump.FullName -AsByteStream
+
+$hash = $hashfunc.ComputeHash($dumpcontent)
+
+$obj = @{
+    hash = $hash
+    data = (Get-Content $lastdump.FullName -Raw | ConvertFrom-Json -AsHashtable)
+}
+
+ConvertTo-Json $obj | Out-File -FilePath "$out/$($lastdump.Name)" -Force
